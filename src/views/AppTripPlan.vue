89071c8299c181bacb786c7a2adec6c28821ccf1
@ydajeong7 <ydajeong7@gmail.com>
2023-05-24 16:39:51 +0900

    Feat: step2, step3 attractionList 분리 (마무리 지어야 함)

--- src/views/AppTripPlan.vue
+++ src/views/AppTripPlan.vue
@@ -57,17 +57,74 @@
                   <v-row v-for="(n, index) in calNumberOfDays" :key="index">
                     <v-tab>{{ index + 1 }}일차</v-tab>
                   </v-row>
-                  <v-tab-item v-for="n in 3" :key="n">
+
+                  <v-tab-item v-for="(n, index) in calNumberOfDays" :key="index">
                     <v-container fluid>
-                      <v-row>
-                        <v-col v-for="i in 6" :key="i" cols="12" md="4">
-                          <v-img
-                            :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
-                            :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
-                            aspect-ratio="1"
-                          ></v-img>
-                        </v-col>
-                      </v-row>
+                      <v-card-text class="py-0">
+                        <v-timeline align-top dense>
+                          <v-timeline-item color="pink" small>
+                            <v-row class="pt-1">
+                              <v-col cols="3">
+                                <strong>5pm</strong>
+                              </v-col>
+                              <v-col>
+                                <strong>New Icon</strong>
+                                <div class="text-caption">Mobile App</div>
+                              </v-col>
+                            </v-row>
+                          </v-timeline-item>
+
+                          <v-timeline-item color="teal lighten-3" small>
+                            <v-row class="pt-1">
+                              <v-col cols="3">
+                                <strong>3-4pm</strong>
+                              </v-col>
+                              <v-col>
+                                <strong>Design Stand Up</strong>
+                                <div class="text-caption mb-2">Hangouts</div>
+                                <v-avatar>
+                                  <v-img
+                                    src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Kurt&hairColor=Red&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Skull&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown"
+                                  ></v-img>
+                                </v-avatar>
+                                <v-avatar>
+                                  <v-img
+                                    src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Prescription02&hairColor=Black&facialHairType=MoustacheMagnum&facialHairColor=BrownDark&clotheType=BlazerSweater&clotheColor=Black&eyeType=Default&eyebrowType=FlatNatural&mouthType=Default&skinColor=Tanned"
+                                  ></v-img>
+                                </v-avatar>
+                                <v-avatar>
+                                  <v-img
+                                    src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Pale"
+                                  ></v-img>
+                                </v-avatar>
+                              </v-col>
+                            </v-row>
+                          </v-timeline-item>
+
+                          <v-timeline-item color="pink" small>
+                            <v-row class="pt-1">
+                              <v-col cols="3">
+                                <strong>12pm</strong>
+                              </v-col>
+                              <v-col>
+                                <strong>Lunch break</strong>
+                              </v-col>
+                            </v-row>
+                          </v-timeline-item>
+
+                          <v-timeline-item color="teal lighten-3" small>
+                            <v-row class="pt-1">
+                              <v-col cols="3">
+                                <strong>9-11am</strong>
+                              </v-col>
+                              <v-col>
+                                <strong>Finish Home Screen</strong>
+                                <div class="text-caption">Web App</div>
+                              </v-col>
+                            </v-row>
+                          </v-timeline-item>
+                        </v-timeline>
+                      </v-card-text>
                     </v-container>
                   </v-tab-item>
                 </v-tabs>
